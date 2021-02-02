738b5682aeaeb1b5d6a161576c99aee75ca9952e
Jeff Adler <JeffM.Adler@gmail.com>
2021-02-02 15:39:47 -0500

    annotation for functions

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -12,3 +12,40 @@ const multiple = function (a: number, b: number): number {
 const divide = (a: number, b: number): number => {
   return a / b;
 };
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+// if a function is not guarenteed to never finish, it will not return type never, since it SOMETIMES could finish
+const throwErrorIf = (message: string): void => {
+  if (message) {
+    throw new Error(message);
+  }
+
+  return;
+};
+
+const todaysWeather = {
+  date: new Date(),
+  weather: 'sunny',
+};
+
+//object destructuring with TS: list of keys point to list of values of specified types
+// destructuring portion is separate from annotation portion
+const logWeather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+console.log(todaysWeather);
