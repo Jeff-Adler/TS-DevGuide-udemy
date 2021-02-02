f5c01ccbdea9e79912c7e939e2b8d69b8248e129
Jeff Adler <JeffM.Adler@gmail.com>
2021-02-02 13:15:13 -0500

    first JSON request

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -1,8 +1,17 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const url = 'https://jsonplaceholder.typicode.com/todos/1'
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url)
-.then(response => {
-  console.log(response.data)
-})
+axios.get(url).then((response) => {
+  const todo = response.data;
+
+  const ID = todo.ID;
+  const title = todo.Title;
+  const finished = todo.finished;
+
+  console.log(` 
+  The Todo with ID: ${ID}
+  Has a title of: ${title}
+  Is it finished? ${finished}
+  `);
+});
