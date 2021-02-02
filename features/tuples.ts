552fcc1ed879e4a7fc4f373e58565e82c0ff9bbc
Jeff Adler <JeffM.Adler@gmail.com>
2021-02-02 18:04:31 -0500

    tuples and type aliasing

---
+++ features/tuples.ts
@@ -0,0 +1,22 @@
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+};
+
+//this will be inferred to be an array of multiple possible types, not preserving the strict ordering we want to maintain
+const pepsi = ['brown', true, 40];
+//We want to prevent:
+pepsi[0] = 40;
+pepsi[2] = 'brown';
+
+//Tuple preserves ordering:
+const coke: [string, boolean, number] = ['brown', true, 40];
+//We want to prevent:
+// coke[0] = 40;
+// coke[2] = 'brown';
+
+//type aliasing is also useful:
+type Drink = [string, boolean, number];
+const sprite: Drink = ['brown', true, 40];
+const fanta: Drink = ['brown', true, 40];
