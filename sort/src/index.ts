a6848afec4c9662ba215c0c197f0653141506de7
Jeff Adler <JeffM.Adler@gmail.com>
2021-02-03 15:01:20 -0500

    implemented numbers collection sort in index

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,29 +1,7 @@
-class Sorter {
-  // This will declare collection field AND initialize it to value passed at New
-  constructor(public collection: number[] | string) {}
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
 
-  sort(): void {
-    const { length } = this.collection;
-
-    // Type guard allows us to run array-specific functions even though collection is of type Array | string
-    if (this.collection instanceof Array) {
-      // Bubble Sort Algorithm
-      for (let i = 0; i < length; i++) {
-        for (let j = 0; j < length - i - 1; j++) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            const leftHand = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = leftHand;
-          }
-        }
-      }
-    }
-
-    if (typeof this.collection === 'string') {
-    }
-  }
-}
-
-const sorter = new Sorter([10, 3, -5, 0]);
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(sorter.collection);
